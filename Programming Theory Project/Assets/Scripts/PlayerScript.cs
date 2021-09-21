8a0fa9cc27d633f8b712849995c436eafb36f9fb
VictorBell <62377686+VictorBell@users.noreply.github.com>
2021-09-21 20:14:21 +0300

    New version

--- Programming Theory Project/Assets/Scripts/PlayerScript.cs
+++ Programming Theory Project/Assets/Scripts/PlayerScript.cs
@@ -8,36 +8,76 @@ public class PlayerScript : MonoBehaviour
     [SerializeField] float rotationSpeed;
     float horizontalInput;
     float verticalInput;
+    [SerializeField]float whenTurning;
+
+    bool isTurning;
     
     Rigidbody playerRb;
-    
+    Animator anim;
+
+    Quaternion toRotate;
+
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Move();
+        Attack();
+        
     }
     void Move()
     {
         //Change later
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
-        Vector3 movementDirection = new Vector3(horizontalInput, verticalInput);
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
         float magnitude = movementDirection.magnitude;
         movementDirection.Normalize();
-
-        playerRb.transform.Translate(movementDirection * speed * Time.deltaTime * magnitude, Space.World);
         
         //Rotate while moving 
         if (movementDirection != Vector3.zero)
         {
-            Quaternion toRotate = Quaternion.LookRotation(movementDirection, Vector3.up);
+            toRotate = Quaternion.LookRotation(movementDirection, Vector3.up);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotate, rotationSpeed * Time.deltaTime);
+            if (toRotate != transform.rotation)
+            {
+                isTurning = true;
+            }
+            else isTurning = false;
+        }
+
+        if(isTurning) {
+            if (whenTurning > 0)
+            {
+            whenTurning -= 0.1f;
+            }
+        }
+        else
+        {
+            if(whenTurning < 1)
+            {
+                whenTurning += 0.1f;
+            }
+            
+        }
+        playerRb.transform.Translate(movementDirection * speed * Time.deltaTime * magnitude * whenTurning, Space.World);
+    }
+
+    void Attack()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            anim.SetBool("isAttacking", true);
+        }
+        else
+        {
+            anim.SetBool("isAttacking", false);
         }
     }
 }
