c03c6b09eb83928d000cea4679fd45815366f0d3
VictorBell <62377686+VictorBell@users.noreply.github.com>
2021-09-28 22:17:38 +0300

    Update

--- Programming Theory Project/Assets/Scripts/Enemy.cs
+++ Programming Theory Project/Assets/Scripts/Enemy.cs
@@ -7,6 +7,10 @@ public class Enemy : MonoBehaviour
     [SerializeField]GameObject enemy;
     GameObject player;
 
+    bool chasingForPlayer = false;
+
+    [SerializeField]float speed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,8 +18,31 @@ public class Enemy : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+      if (chasingForPlayer)
+        {
+
+            transform.LookAt (new Vector3 (player.transform.position.x, transform.position.y, player.transform.position.z));
+            Vector3 lookDirection = (player.transform.position - enemy.transform.position).normalized;
+            transform.Translate(lookDirection * speed * Time.deltaTime);
+        }   
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            player = collision.gameObject;
+            chasingForPlayer = true;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            player = other.gameObject;
+            chasingForPlayer = true;
+        }
     }
 }
